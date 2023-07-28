ef1c035e928e97959fe33f7637832494d80736d7
Kaen Pawanachai <pawaox4th@gmail.com>
2023-07-28 14:38:14 +0700

    feat(tools): install prettier for format code

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/**
+ * @link https://gist.github.com/PawaOx4th/9d3d3166c3fa6866966cbfdb0e96091f
+ */
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 60,
+  singleQuote: false,
+  trailingComma: "all",
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+};
