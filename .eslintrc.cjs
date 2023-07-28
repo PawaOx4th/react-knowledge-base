c68256a83334923777727ecfa357ff537496b9b5
Kaen Pawanachai <pawaox4th@gmail.com>
2023-07-28 14:52:24 +0700

    build(shared): add parserOptions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,17 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   root: true,
   env: { browser: true, es2020: true },
+
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
